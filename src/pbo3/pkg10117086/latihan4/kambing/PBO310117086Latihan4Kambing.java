51b63847d6bdd851a1f736006bfe67e9b984ca19
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:11:29 -0700

    kambing

---
+++ src/pbo3/pkg10117086/latihan4/kambing/PBO310117086Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+
+ */
+package pbo3.pkg10117086.latihan4.kambing;
+
+/**
+ *
+ * @author Lenovo
+ * Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan4Kambing {
+            public void tambahKambing(){
+            //Deklarasi variabel lokal
+            int jumlahKambing = 0;
+            
+            jumlahKambing = jumlahKambing + 5;
+                System.out.println("Jumlah Kambing Setelah Ditambah: "+
+                        jumlahKambing);
+            }
+    
+
+    
+    public static void main(String[] args) {
+       PBO310117086Latihan4Kambing kambingJantan = new
+            PBO310117086Latihan4Kambing();
+       kambingJantan.tambahKambing();
+            
+    }
+    
+}
